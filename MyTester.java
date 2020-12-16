8df07f9b6b591d3feef79eefaf9be1d21176415f
ava-lu <alu21@stuy.edu>
2020-12-16 15:47:24 -0500

    success sorting small array

--- MyTester.java
+++ MyTester.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 public class MyTester {
   public static void main(String[] args) {
     int[] arr = new int[]{1,3,2,6,5};
     Sorts.bubbleSort(arr);
-    System.out.println(arr);
+    System.out.println(Arrays.toString(arr));
   }
 }
--- Sorts.java
+++ Sorts.java
@@ -5,7 +5,7 @@ public class Sorts {
   */
   public static void bubbleSort(int[] data) {
     for (int i=data.length-1; i>=0; i--) {
-      for (int j=0; j<data.length; j++) {
+      for (int j=0; j<data.length-1-i; j++) {
         if (data[j]>data[j+1]) {
           int temp = data[j];
           data[j] = data[j+1];
