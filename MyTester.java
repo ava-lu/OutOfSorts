e41fee10b3c99467da17acf7225d41225b926068
ava-lu <alu21@stuy.edu>
2020-12-16 19:20:53 -0500

    debugged

--- MyTester.java
+++ MyTester.java
@@ -5,13 +5,15 @@ import java.util.Collections;
 public class MyTester {
   public static void main(String[] args) {
     //small array test
-    // int[] arr = new int[]{1,3,2,6,5};
-    // Sorts.bubbleSort(arr);
-    // System.out.println(Arrays.toString(arr));
+    int[] arr = new int[]{1,3,2,6,5};
+    int[] arr2 = new int[]{};
+    Sorts.bubbleSort(arr);
+    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.toString(arr2));
 
     //random array test
     Random rng = new Random();
-    for (int i=0; i<20; i++) {
+    for (int i=0; i<10; i++) {
       int size = rng.nextInt(100);
       boolean passes = false;
       System.out.println("Tester case "+ i);
@@ -24,11 +26,10 @@ public class MyTester {
         correctarr[j]=element;
       }
       Sorts.bubbleSort(tester);
+      //System.out.println(Arrays.toString(tester));
       Arrays.sort(correctarr);
-      String myresult = Arrays.toString(tester);
-      String correct = Arrays.toString(correctarr);
-      if (myresult.equals(correct)) passes=true;
-      if (passes=true) System.out.println(" passes.");
+      if (Arrays.equals(tester, correctarr)) passes=true;
+      if (passes) System.out.println(" passes.");
       else System.out.println(" fails.");
     }
   }
--- Sorts.java
+++ Sorts.java
@@ -4,7 +4,7 @@ public class Sorts {
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data) {
-    for (int i=data.length-1; i>=0; i--) {
+    for (int i=0; i<data.length; i++) {
       for (int j=0; j<data.length-1-i; j++) {
         if (data[j]>data[j+1]) {
           int temp = data[j];
