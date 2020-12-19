54f1e798f21520157dc8f6985f9bb60b0be13742
ava-lu <alu21@stuy.edu>
2020-12-19 15:08:54 -0500

    random array tests on selectionSort

--- MyTester.java
+++ MyTester.java
@@ -36,8 +36,30 @@ public class MyTester {
     // }
 
     //selectionSort
-    int[] arr = new int[]{54,53,32,1,2};
-    Sorts.selectionSort(arr);
-    System.out.println(Arrays.toString(arr));
+    //small array test
+    // int[] arr = new int[]{54,53,32,1,2};
+    // Sorts.selectionSort(arr);
+    // System.out.println(Arrays.toString(arr));
+
+    Random rng = new Random();
+    for (int i=0; i<=20; i++) {
+      int size = rng.nextInt(100);
+      boolean passes = false;
+      System.out.println("Tester case "+ i);
+      int[] tester = new int[size];
+      int[] correctarr = new int[size];
+      for (int j=0; j<size; j++) {
+        Random ele = new Random();
+        int element = ele.nextInt(100);
+        tester[j]=element;
+        correctarr[j]=element;
+     }
+      Sorts.selectionSort(tester);
+      System.out.println(Arrays.toString(tester));
+      Arrays.sort(correctarr);
+      if (Arrays.equals(tester, correctarr)) passes=true;
+      if (passes) System.out.println(" passes.");
+      else System.out.println(" fails.");
+    }
   }
 }
